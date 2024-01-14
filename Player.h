79509623f607d6049b1cf4abad8eb9bcc1399a81
Margherita Cattapan <96427752+MackAt29@users.noreply.github.com>
2024-01-14 22:29:09 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,48 +3,38 @@
 
 #include "Property.h"
 #include <iostream>
-#include <vector>
-//#include "property.h"  // Includi la definizione della classe Property (probabilmente sarà casella)
-
-class Property;
+#include <vector> 
 
 class Player {
-public:
+private:
     char name;
-    //std::vector<Property> ownedProperties;
     bool isHuman;
     int gold;
-    //const Property* currentLocation; // Puntatore alla casella corrente
     int currentLocation;
+    bool hasLost;
     int initTurn;
-    bool HasLost; // Flag per indicare se il giocatore ha perso
-    int boardSize = 28;
 
-    Player(const char& playerName, bool human);
-    // Costruttore di copia
-    Player(const Player& other);
-
-    //metodo per la scelta del turno
+    //lancio dadi
     int getDiceRoll();
-    //metodo lancio dadi
-    int rollDice();
-    //inizializzazione e settaggio della posizione del giocatore
+    //settaggio della posizione del giocatore
     void setPosition(int diceResult);
+    //recupero posizione del giocatore
+    int getPosition(){return currentLocation;}
     //Recupero esito del primo tiro di dadi()
     int getInitTurn(){return initTurn;}
-    //recupero informazioni sulla cella pari alla positzione del player
-    //const Property* getPosition() const;
-    
-    //effetuare il turno con tiro di due dadi e eventuale mossa di acquisto o pagamento
-    void takeTurn();
-    
-    //pagamento affitto sulla casella
-    //void payPlayer(const Player& propertyOwner, int rentAmount);
-    
-private:
-    void move(int steps);
+    //mossa pedina e aggiunta gold per passaggio al via
+    void move(int steps, const Board& board);
+    //pagamento affitto casella
+    void payPlayer(const Player& propertyOwner, int rentAmount);
+    //check gold disponibile
     bool checkBalance(int amount);
-    void humanChoice();
+
+public:
+    Player(char playerName, bool human);
+    // Costruttore di copia
+    Player(const Player& other);
+    //turno del giocatore
+    void takeTurn(const Board& board);
 };
 
 #endif  // PLAYER_H
