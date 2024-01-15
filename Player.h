871a81fcf1888ca18bfa5693486545354108b89a
Margherita Cattapan <96427752+MackAt29@users.noreply.github.com>
2024-01-15 16:57:12 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,9 +1,17 @@
+//Margherita Cattapan 2008798
 #ifndef PLAYER_H
 #define PLAYER_H
 
 #include "Property.h"
+#include <vector>
 #include <iostream>
-#include <vector> 
+#include <fstream>
+#include <string>
+#include <algorithm>    // std::random_shuffle
+#include <ctime>        // std::time
+#include <cstdlib>      // std::rand, std::srand
+
+class Property;
 
 class Player {
 private:
@@ -13,28 +21,39 @@ private:
     int currentLocation;
     bool hasLost;
     int initTurn;
+    std::vector<Property> ownList_;
 
     //lancio dadi
     int getDiceRoll();
     //settaggio della posizione del giocatore
     void setPosition(int diceResult);
-    //recupero posizione del giocatore
-    int getPosition(){return currentLocation;}
-    //Recupero esito del primo tiro di dadi()
-    int getInitTurn(){return initTurn;}
     //mossa pedina e aggiunta gold per passaggio al via
     void move(int steps);
-    //pagamento affitto casella
-    void payPlayer(const Player& propertyOwner, int rentAmount);
     //check gold disponibile
     bool checkBalance(int amount);
 
 public:
-    Player(char playerName, bool human);
+    Player(const char& playerName, bool human);
     // Costruttore di copia
     Player(const Player& other);
     //turno del giocatore
-    void takeTurn(const Board& board);
+    //void takeTurn(Board& board);
+    //output su file
+    void logToFile(const std::string& message) const;
+    void advance();
+    char getName(){return name;}
+    bool isHumanPlayer(){return isHuman;}
+    //Recupero esito del primo tiro di dadi()
+    int getInitTurn(){return initTurn;}
+    //check gold player
+    int getGold(){return gold;}
+    void payProperty(int priceAmount);
+    void acquireProperty(Property& land);
+    //recupero posizione del giocatore
+    int getPosition(){return currentLocation;}
+    //pagamento affitto casella
+    void payPlayer(Player* propertyOwner, int rentAmount);
+    void getOwnedList();
 };
 
 #endif  // PLAYER_H
